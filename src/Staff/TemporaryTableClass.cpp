6aae3a16175b9925a7e5091606a6dfcf19a6e159
Selepe Sello <sbkskhalo.kq@gmail.com>
2023-11-01 13:34:16 +0200

    The Table Component Class

---
+++ src/Staff/TemporaryTableClass.cpp
@@ -0,0 +1,9 @@
+#include "TemporaryTableClass.h"
+
+// TODO: Will Be Deleted
+
+TemporaryTableClass::TemporaryTableClass(int id) : id(id) {}
+
+int TemporaryTableClass::getId() {
+    return this->id;
+}
