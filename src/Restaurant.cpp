a0e086b35c8671dba8119922a1f3e46f999f0245
MulisaMusehane <127021696+MulisaMusehane@users.noreply.github.com>
2023-11-03 18:42:43 +0200

    Implementation for the Restaurant Class
    Added complete implementation for the restaurant class and functions

--- src/Restaurant.cpp
+++ src/Restaurant.cpp
@@ -1,6 +1,10 @@
 #include "Restaurant.h"
 
-Restaurant :: Restaurant(){
+Restaurant :: Restaurant() 
+ :  waiterManager(5, &kitchen),
+    tables(20),
+    customerIntake(10) {
+    frame = 0;
     tabs = new TabStore();
 }
 
@@ -15,4 +19,41 @@ Restaurant& Restaurant :: instance(){
 
 TabStore* Restaurant :: getTabStore(){
     return tabs;
+}
+
+int Restaurant :: getFrame() {
+    return frame;
+}
+
+int Restaurant::getFrame() {
+    bool isBooking;
+    int numCustomers;
+
+    if (numCustomers = customerIntake.check(&isBooking)) {
+        if (tables.useTables(numCustomers)) {
+            if (isBooking) {
+                reservations.setBookingStrategy(new Reserved(&reservations));
+            } else {
+                reservations.setBookingStrategy(new WalkIn(&reservations));
+            }
+
+            reservations.addBooking(numCustomers);
+        }
+    }
+
+    Booking* booking = NULL;
+    if (booking = reservations.checkBookings()) {
+        TableComponent* table = tables.getTables(booking->numCustomers);
+        waiterManager.assignTable(table);
+    }
+
+    waiterManager.serve();
+    staffCheckup.checkup();
+
+    cout << tables.toString();
+    cout << kitchen.toString();
+
+    waiterManager.progressWaiters();
+    staffCheckup.progressCheckup();
+    frame++;
 }
