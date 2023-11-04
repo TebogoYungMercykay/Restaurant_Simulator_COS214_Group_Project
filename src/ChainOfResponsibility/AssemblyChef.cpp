96e614f862285dc02d8ae31b7d0aaeb9364bc47f
Shama <62930548+Shama0108@users.noreply.github.com>
2023-11-04 20:23:15 +0200

    Update: Removed the takeaway option from the COF

--- src/ChainOfResponsibility/AssemblyChef.cpp
+++ src/ChainOfResponsibility/AssemblyChef.cpp
@@ -6,11 +6,7 @@ AssemblyChef::AssemblyChef() {
 
 void AssemblyChef::prepareDish(Order* dish) {
     if (dish != nullptr) {
-        if (dish->contains("takeaway")) {
-            std::cout << "\tAssembly Chef Packaging the " << dish->getMeal() << " Accurately and Efficiently." << std::endl;
-        } else if (dish->contains("dine-in")) {
-            std::cout << "\tAssembly Chef Plating the " << dish->getMeal() << " Accurately and Efficiently." << std::endl;
-        }
+        std::cout << "\tAssembly Chef Plating the " << dish->getMeal() << " Accurately and Efficiently." << std::endl;
         if (this->nextStation != nullptr) {
             this->nextStation->prepareDish(dish);
         } else {
