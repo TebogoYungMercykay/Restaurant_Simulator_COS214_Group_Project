233c0f21358e98f9e860f7bcf4ad5c66ee0bfa68
hlokomani-tuks <u21488593@tuks.co.za>
2023-11-04 10:05:04 +0200

    Fixed typos and included iostream

--- src/State/Waiting.cpp
+++ src/State/Waiting.cpp
@@ -1,6 +1,7 @@
 #include "Waiting.h"
 #include <cstdlib>
 #include <ctime>
+#include <iostream>
 
 void Waiting :: setTableComponent(TableComponent* table) {
     this->table = table;
@@ -10,6 +11,6 @@ void Waiting :: serve() {
     srand(time(0));
     // Random number between 23 and 150
     int random_number = rand() % (128) + 23;
-    std::cout << "Your order number is #" << random_number << ". You can use this number to track the status of your order." << std::end;
+    std::cout << "Your order number is #" << random_number << ". You can use this number to track the status of your order." << std::endl;
     this->table->changeState(new Paying());
 }
