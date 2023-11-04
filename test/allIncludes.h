040fc5270545d580ac4ce2635d42ba24896f04be
hlokomani-tuks <u21488593@tuks.co.za>
2023-11-04 19:46:42 +0200

    Created one include file for all test cpp's

---
+++ test/allIncludes.h
@@ -0,0 +1,25 @@
+#ifndef ALLINCLUDES_H
+#define ALLINCLUDES_H
+
+#include <iostream>
+#include <vector>
+#include <gtest/gtest.h>
+#include <stdexcept>
+#include <limits.h>
+
+#include "../src/Staff/StaffCheckup.h"
+#include "../src/ChainOfResponsibility/KitchenManager.h"
+#include "../src/Builder/Order.h"
+#include "../src/Builder/OrderItem.h"
+#include "../src/Builder/OrderDirector.h"
+#include "../src/Builder/WackNuggetsBuilder.h"
+#include "../src/Builder/WackCrispyBuilder.h"
+#include "../src/Builder/KotaPounderBuilder.h"
+#include "../src/Builder/HugeMacBuilder.h"
+#include "../src/CustomerSession.h"
+#include "../src/Bill/SingleBill.h"
+#include "../src/Bill/SplitBill.h"
+#include "../src/Tab/Tab.h"
+#include "../src/Tab/TabStore.h"
+
+#endif
