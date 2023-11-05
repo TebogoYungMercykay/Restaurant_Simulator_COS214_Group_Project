75d9afbc2b88a08a0f0acd4222777ade00b408a9
Shama <62930548+Shama0108@users.noreply.github.com>
2023-11-05 16:08:14 +0200

    Update: Make use of the getOrdersMethod

--- src/ChainOfResponsibility/KitchenStaff.cpp
+++ src/ChainOfResponsibility/KitchenStaff.cpp
@@ -7,7 +7,14 @@ KitchenStaff::KitchenStaff() {
 void KitchenStaff::prepareDish(Order* dish) {
     if (dish != nullptr) {
         std::cout << "Order for Table " << dish->getTable() << " being handled" << std::endl;
-        std::cout << "\tKitchen Staff is preparing the " << dish->toStringSideItems() << std::endl;
+        std::cout << "\tKitchen Staff is preparing the "
+        std::vector<OrderItem> items = dish->getOrderItems("side");
+        if (items.size() != 0) {
+            for (int k = 0; k < items.size(); k++) {
+                std::cout << (items[k]).getName();
+            }
+            std::cout << std::endl;
+        }
         if (this->nextStation != nullptr) {
             this->nextStation->prepareDish(dish);
         } else {
