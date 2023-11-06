8d7ab2021a605c26b5a76796a518620b180094f3
tebogoyungmercykay <sbkskhalo.kq@gmail.com>
2023-11-06 20:42:16 +0000

    Linter: Applied clang-format and clang-tidy Fixes

--- src/Restaurant.cpp
+++ src/Restaurant.cpp
@@ -1,110 +1,90 @@
 #include "Restaurant.h"
 
-Restaurant :: Restaurant() 
- :  waiterManager(5, &kitchen),
-    tables(20),
-    customerIntake(2),
-    staffCheckup(1, waiterManager.getIterators()) {
-    this->frame = 0;
-    this->revenue = 0;
-    this->tip = 0;
-    this->tabs = new TabStore();
+Restaurant ::Restaurant()
+    : waiterManager(5, &kitchen), tables(20), customerIntake(2),
+      staffCheckup(1, waiterManager.getIterators()) {
+  this->frame = 0;
+  this->revenue = 0;
+  this->tip = 0;
+  this->tabs = new TabStore();
 }
 
-Restaurant :: ~Restaurant(){
-    delete tabs;
+Restaurant ::~Restaurant() { delete tabs; }
+
+Restaurant &Restaurant ::instance() {
+  static Restaurant onlyInstance_;
+  return onlyInstance_;
 }
 
-Restaurant& Restaurant :: instance(){
-    static Restaurant onlyInstance_;
-    return onlyInstance_;
-}
+TabStore *Restaurant ::getTabStore() { return tabs; }
 
-TabStore* Restaurant :: getTabStore(){
-    return tabs;
-}
-
-int Restaurant :: getFrame() {
-    return frame;
-}
+int Restaurant ::getFrame() { return frame; }
 
 void Restaurant::printStats() {
-    int hours = frame/12;
-    int minutes = (frame - (hours * 12)) * 5;
+  int hours = frame / 12;
+  int minutes = (frame - (hours * 12)) * 5;
 
-    printf(
-        "\nCurrent frame: %d\n"
-        "Current time: %.2d:%.2d\n\n"
+  printf("\nCurrent frame: %d\n"
+         "Current time: %.2d:%.2d\n\n"
 
-        "Number of tables remaining: %d/%d\n\n"
+         "Number of tables remaining: %d/%d\n\n"
 
-        "Total tips: %.2f\n"
-        "Total Revenue: %.2f\n\n",
+         "Total tips: %.2f\n"
+         "Total Revenue: %.2f\n\n",
 
-        frame, 
-        hours, minutes, 
+         frame, hours, minutes,
 
-        tables.getNumRemaining(), tables.getNumTables(),
+         tables.getNumRemaining(), tables.getNumTables(),
 
-        tip,
-        revenue
-    );
+         tip, revenue);
 }
 
 void Restaurant::nextFrame() {
-    printStats();
+  printStats();
 
-    bool isBooking;
-    int numCustomers;
+  bool isBooking;
+  int numCustomers;
 
-    if (numCustomers = customerIntake.check(&isBooking)) {
-        if (tables.useTables(numCustomers)) {
-            if (isBooking) {
-                reservations.setBookingStrategy(new Reserved(&reservations));
-            } else {
-                reservations.setBookingStrategy(new WalkIn(&reservations));
-            }
+  if (numCustomers = customerIntake.check(&isBooking)) {
+    if (tables.useTables(numCustomers)) {
+      if (isBooking) {
+        reservations.setBookingStrategy(new Reserved(&reservations));
+      } else {
+        reservations.setBookingStrategy(new WalkIn(&reservations));
+      }
 
-            reservations.addBooking(numCustomers);
-        }
+      reservations.addBooking(numCustomers);
     }
+  }
 
-    Booking* booking = NULL;
-    if (booking = reservations.checkBookings()) {
-        TableComponent* table = tables.getTables(booking->numCustomers);
-        table->setCustomerName(booking->customerName);
-        table->setNumCustomers(booking->numCustomers);
-        waiterManager.assignTable(table);
+  Booking *booking = NULL;
+  if (booking = reservations.checkBookings()) {
+    TableComponent *table = tables.getTables(booking->numCustomers);
+    table->setCustomerName(booking->customerName);
+    table->setNumCustomers(booking->numCustomers);
+    waiterManager.assignTable(table);
 
-        delete booking;
-    }
+    delete booking;
+  }
 
-    staffCheckup.progressCheckup();
-    waiterManager.progressWaiters();
+  staffCheckup.progressCheckup();
+  waiterManager.progressWaiters();
 
-    cout << waiterManager.toString();
-    cout << "\n\n";
-    cout << kitchen.toString();
+  cout << waiterManager.toString();
+  cout << "\n\n";
+  cout << kitchen.toString();
 
-    waiterManager.serve();
+  waiterManager.serve();
 
-    kitchen.progressKitchen();
+  kitchen.progressKitchen();
 
-    progressFrame();
+  progressFrame();
 }
 
-void Restaurant::progressFrame() {
-    frame++;
-}
+void Restaurant::progressFrame() { frame++; }
 
-void Restaurant :: addTip(double amount){
-    tip += amount;
-}
+void Restaurant ::addTip(double amount) { tip += amount; }
 
-void Restaurant :: addRevenue(double amount){
-    revenue += amount;
-}
+void Restaurant ::addRevenue(double amount) { revenue += amount; }
 
-void Restaurant :: addComplaint(string comp){
-    complaints.push_back(comp);
-}
+void Restaurant ::addComplaint(string comp) { complaints.push_back(comp); }
