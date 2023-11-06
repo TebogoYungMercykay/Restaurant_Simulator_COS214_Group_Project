1218421fc110b62d6583b8d331b3f3bfaeb18af3
Sibisi Mathebe <sibisirobert12@gmail.com>
2023-11-06 10:43:49 +0200

    Merged Waiting.cpp

--- src/State/Waiting.cpp
+++ src/State/Waiting.cpp
@@ -1,6 +1,7 @@
 #include "Waiting.h"
 #include <cstdlib>
 #include <ctime>
+#include "../CustomerSession.h"
 
 void Waiting::setTableComponent(TableComponent* table) {
     this->table = table;
@@ -11,7 +12,9 @@ void Waiting::serve() {
     // Random number between 23 and 150
     int random_number = rand() % (128) + 23;
     std::cout << "Your order number is #" << random_number << ". You can use this number to track the status of your order." << std::endl;
-    this->table->changeState(new Paying());
+    Paying* var = new Paying();
+    var->setTableComponent(this->table);
+    this->table->changeState(var);
     this->table->getCustomerSession()->getOrder()->setOrderNum(random_number);
 }
 
