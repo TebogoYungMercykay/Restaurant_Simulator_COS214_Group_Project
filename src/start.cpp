4d26f90c4310e2d17179224d79c536fdd6bfcda2
Sibisi Mathebe <sibisirobert12@gmail.com>
2023-11-06 11:15:39 +0200

    Added start file to run simulation

---
+++ src/start.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "Restaurant.h"
+
+using namespace std;
+
+int main() {
+    char input = '\0';
+
+    while (input != 'q') {
+        Restaurant::instance().nextFrame();
+        
+        cout << "\n\n(Press Enter for next or q to quit) ";
+        cout << "\n------------------------------------- ";
+        
+        input = getc(stdin);
+    }
+}
